538dfa906b230a7869ce510f93fb8b418d7de8cf
nikul <nikularcade@gmail.com>
2026-09-11 23:54:52 +0530

    Initial commit: basic website

---
+++ build/OpenPLC Simulator/precompile/obj/pou_FB_MOTOR.cpp.d
@@ -0,0 +1,34 @@
+C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\precompile\obj\pou_FB_MOTOR.cpp.o: \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\precompile\sources\pou_FB_MOTOR.cpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/generated.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_types.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cstdint \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cstddef \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_var.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/type_traits \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/utility \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_global.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_array.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/array \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_fault.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_located.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_std_lib.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_traits.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_retain.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_ptr.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_string.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cstdio \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cstring \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cstdlib \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/algorithm \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_wstring.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_time.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_date.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_dt.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_tod.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/cmath \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/chrono \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_enum.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_memory.hpp \
+ C:\PLC\OpenPLC_BatchProcessingPlant\build\OpenPLC\ Simulator\src/iec_pointer.hpp \
+ C:\Users\suraj\AppData\Local\Temp\openplc-avr-libstdcpp-4.2.11\include/string
---
+++ build/OpenPLC Simulator/src/defines.h
@@ -0,0 +1,26 @@
+
+
+//Program MD5
+#define PROGRAM_MD5 "d21821955ad7b2efed5a873e16938899"
+
+//Comms Configuration
+#define SIMULATOR_MODE
+#define MBSERIAL_IFACE Serial
+#define MBSERIAL_BAUD 115200
+#define MBSERIAL_SLAVE 1
+#define MBSERIAL
+#define MODBUS_ENABLED
+
+
+//IO Config
+#define PINMASK_DIN 
+#define PINMASK_AIN 
+#define PINMASK_DOUT 
+#define PINMASK_AOUT 
+#define NUM_DISCRETE_INPUT 0
+#define NUM_ANALOG_INPUT 0
+#define NUM_DISCRETE_OUTPUT 0
+#define NUM_ANALOG_OUTPUT 0
+
+
+//Arduino libraries
